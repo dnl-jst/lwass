3ecc8d482860472ba1451c73ef20708b5dd9b3fa
Daniel Jost <info@daniel-jost.de>
2017-05-10 21:26:45 +0200

    support very basic smtp forwarding

---
+++ index.js
@@ -0,0 +1,91 @@
+const SMTPServer = require('smtp-server').SMTPServer;
+const SMTPConnection = require('nodemailer/lib/smtp-connection');
+const dns = require('dns');
+const concat = require('concat-stream')
+
+const server = new SMTPServer({
+  authOptional: true,
+  logger: true,
+  onMailFrom: function (address, session, callback) {
+
+    // currently accept any from address
+    callback();
+  },
+  onRcptTo: function (address, session, callback) {
+
+    var domain = address.address.substr(address.address.indexOf('@') + 1);
+
+    dns.resolveSrv('_lwass._tcp.' + domain, function(err, addresses) {
+
+      if (err) {
+
+        console.log(err);
+        callback(new Error('internal server error'));
+
+      } else {
+
+        if (!addresses) {
+
+          callback(new Error('target address doesnt support lwass'));
+
+        } else {
+
+          // target smtp
+          session.targetSmtpHostname = addresses[0].name;
+          session.targetSmtpPort = addresses[0].port;
+
+          callback();
+        }
+      }
+    });
+  },
+  onData: function (stream, session, callback) {
+
+    var concatStream = concat(function(data) {
+
+      var connection = new SMTPConnection({
+        host: session.targetSmtpHostname,
+        port: session.targetSmtpPort,
+        logger: true
+      });
+
+      connection.on('error', function(err) {
+
+        var returnError = new Error(err.response);
+        returnError.responseCode = err.responseCode;
+
+        callback(returnError);
+      });
+
+      connection.connect(function() {
+
+        var envelope = {
+          from: session.envelope.mailFrom.address,
+          to: session.envelope.rcptTo[0].address
+        };
+
+        connection.send(envelope, data, function(err, info) {
+
+          if (err) {
+
+            var returnError = new Error(err.response);
+            returnError.responseCode = err.responseCode;
+
+            connection.close();
+
+            callback(returnError);
+          } else {
+
+            connection.quit();
+            callback();
+          }
+
+        });
+      });
+    });
+
+    stream.pipe(concatStream);
+  }
+});
+
+server.listen(25);
